d6c426dafe7e79546a90b0809107409c3792a4b5
Brittiny Filbert <britfilbert@icloud.com>
2023-08-07 17:20:25 -0700

    Completed losing_node

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,22 +1,43 @@
 require_relative 'tic_tac_toe'
 
+
 class TicTacToeNode
-  def initialize(board, next_mover_mark, prev_move_pos = nil)
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
+  def initialize(board, mark, prev_move_pos = nil)
     @board = board
-    @next_mover_mark = next_mover_mark
+    @next_mover_mark = mark
     @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
+  def losing_node?(evaluator) # evaluator: mark
     # board.over? 
     # winner == opponent (LOSING NODE)
     # winner == nil || player (NOT LOSING NODE)
 
-    loser = true
+    # loser = true
+    # if @board.over?
+    #   @board.winner == @next_mover_mark ? true : false
+    # end
     if @board.over?
-      @board.winner == @next_mover_mark ? true : false
-  
+      if (@board.winner == evaluator) || (@board.tied?)
+        return false
+      else
+        return true
+      end
     end
+   
+    if evaluator == next_mover_mark
+      self.children.all? do |child|
+        child.losing_node?(evaluator)
+      end
+    else
+      self.children.any? do |child|
+        child.losing_node?(evaluator)
+      end
+    end
+    
   end
 
   def winning_node?(evaluator)
@@ -27,15 +48,15 @@ class TicTacToeNode
   def children
     mark = switch_mark
     moves = []
-    (0...@board.length).each do |i|
-      (0...@board.length).each do |j|
+    (0...@board.rows.length).each do |i|
+      (0...@board.rows.length).each do |j|
         duped = @board.dup
 
-        if @board.empty([i,j]) # true if nil/empty
+        if @board.empty?([i,j]) # true if nil/empty
           # game state of other player
           moves << TicTacToeNode.new(duped, mark, [i,j])
           # other players mark
-          duped[i,j]=next_mover_mark
+          duped[[i,j]]= next_mover_mark
         end
         
       end
